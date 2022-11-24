810c7ba81f8c5e1dde5c4320214bc74e4c97640f
American-Academy-IT <dev@theschoolranking.com>
2022-11-24 20:48:44 +0200

    pay v09 change 4

--- scripts/config/config.js
+++ scripts/config/config.js
@@ -4,10 +4,10 @@ CONFIG.JWT = {
 }
 CONFIG.MODE = 'PROD';
 CONFIG.PROD_MODE = CONFIG.MODE === 'DEV' ? false: true;
-CONFIG.IS_CERT_AUTH_ENABLED = true;
+CONFIG.IS_CERT_AUTH_ENABLED = false;
 CONFIG.CURRENCY= process.env.CURRENCY_LABEL;
 CONFIG.TEST_GATEWAY = {
-    BASEURL: process.env.TEST_GATEWAY_URL,
+    BASEURL: process.env.BASEURL,
     API_VERSION: process.env.API_VERSION,
     USERNAME: 'merchant.' + process.env.MERCHANT,
     PASSWORD: process.env.PASSWORD ,
